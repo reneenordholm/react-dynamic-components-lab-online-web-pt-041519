b81725ddb28e3494be568a888495773ec73fe9d4
Renee Nordholm <reneenordholm@gmail.com>
2020-01-18 00:15:24 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,7 +5,7 @@ export default class ColorBox extends Component {
   render() {
     return (
       <div className="color-box" style={{opacity: 2}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
-        {/* your conditional code here! */}
+        {this.props.opacity}
       </div>
     )
   }
