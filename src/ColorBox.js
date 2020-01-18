62543a3ef576640ad243162b7a2de37c443cfea8
Renee Nordholm <reneenordholm@gmail.com>
2020-01-18 00:32:34 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
-    return (
+    const newOpacity = this.props.opacity - 0.1;
+    return this.props.opacity > 10 ? null : (
       <div className="color-box" style={{opacity: 2}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
-        {this.props.opacity}
+        {this.props.newOpacity}
       </div>
     )
+    // return (
+    //   <div className="color-box" style={{opacity: 2}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
+    //     {this.props.opacity}
+    //   </div>
+    // )
   }
 
 }
